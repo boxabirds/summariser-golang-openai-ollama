712340e322673ec250362fa50cd27ebd2733a342
Julian Harris <julian.harris@gmail.com>
2024-05-28 12:09:29 +0100

    demo of using seed to get deterministic output

--- main-openai.go
+++ main-openai.go
@@ -14,47 +14,25 @@ import (
 
 func main() {
 	const DEFAULT_OPENAI_MODEL = "gpt-3.5-turbo"
-	const DEFAULT_GEMMA_MODEL = "gemma:2b"
-	const OLLAMA_API_KEY = "ollama"
 	const NO_SEED = -1
 	inputFile := flag.String("input-file", "", "Path to the input text file")
 	inputText := flag.String("input-text", "", "Input text to summarize")
 	model := flag.String("model", "", "Model to use for the API")
-	baseURL := flag.String("base-url", "", "Base URL for the API server")
 	maxTokens := flag.Int("max-tokens", 200, "Maximum number of tokens in the summary")
 	var seed int
 	flag.IntVar(&seed, "seed", NO_SEED, "Seed for deterministic results (optional)")
 	flag.Parse()
 
 	var apiKey string
-	if *baseURL == "" {
-		apiKey = os.Getenv("OPENAI_API_KEY")
-		if apiKey == "" {
-			log.Fatal("OPENAI_API_KEY not found in environment")
-		}
-		if *model == "" {
-			*model = DEFAULT_OPENAI_MODEL
-		}
-		fmt.Printf("Using default OpenAI API server\n")
-	} else {
-		apiKey = OLLAMA_API_KEY
-		if *model == "" {
-			*model = DEFAULT_GEMMA_MODEL
-		}
-		fmt.Printf("Using custom API server at: %s\n", *baseURL)
-		fmt.Printf("API Key set to Ollama\n")
+	apiKey = os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		log.Fatal("OPENAI_API_KEY not found in environment")
 	}
-	fmt.Printf("Model being used: %s\n", *model)
-
-	config := openai.DefaultConfig(apiKey)
-
-	// have to check twice because the config that's created and depends on it
-	// and yet needs to be changed again
-	if *baseURL != "" {
-		config.BaseURL = *baseURL
+	if *model == "" {
+		*model = DEFAULT_OPENAI_MODEL
 	}
 
-	client := openai.NewClientWithConfig(config)
+	client := openai.NewClient(apiKey)
 	ctx := context.Background()
 	systemPrompt := `You are a text summarization assistant. 
 	Generate a concise summary of the given input text while preserving the key information and main points. 
@@ -100,8 +78,6 @@ func main() {
 	var content string
 	var completionTokens int
 
-	// My tests are showing that SystemFingerprint is set to nil -- something for a future investigation.
-	//var systemFingerprint string
 	for {
 		response, err := stream.Recv()
 		if err != nil {
@@ -114,13 +90,7 @@ func main() {
 		fmt.Printf(response.Choices[0].Delta.Content)
 		completionTokens += len(response.Choices[0].Delta.Content)
 
-		//if response.SystemFingerprint != "" {
-		//	systemFingerprint = response.SystemFingerprint
-		//	fmt.Printf("System fingerprint: %s\n", response.SystemFingerprint)
-		//}
-
 	}
-	//fmt.Printf("\nFINAL System fingerprint: '%s'\n", systemFingerprint)
 
 	elapsed := time.Since(start)
 	fmt.Printf("\n\nTokens generated: %d\n", completionTokens)
